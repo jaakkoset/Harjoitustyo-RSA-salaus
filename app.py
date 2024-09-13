199911063b7e9b998111aad2d66cca40aba76abe
Jaakko Airikkala <jaakkel@gmail.com>
2024-09-13 22:24:36 +0300

    Add 4 out of 5 steps of key generation

--- app.py
+++ app.py
@@ -2,37 +2,98 @@ from random import randint
 
 class Program:
     def __init__(self):
-        self.prime_q = None
-        self.prime_p = None
+        self.key = Key()
         self.program()
 
     def program(self):
         while True:
             print()
             print("Komennot")
-            print(" 1 Luo uudet satunnaiset alkuluvut.")
-            print(" 2 Katso alkuluvut.")
+            print(" 1 Luo satunnainen salausavain.")
+            print(" 2 Luo salausavain omista alkuluvuista.")
+            print(" 3 Tulosta salausavaimen osat.")
             print(" q Lopeta ohjelma.")
 
             cmd = input("Anna komento: ")
             if cmd == "q":
                 break
-            elif cmd == "1":
-                self.prime_p = Prime().random_prime()
-                self.prime_q = Prime().random_prime()
-            elif cmd == "2":
-                print()
-                print("q: ", self.prime_q)
-                print("p: ", self.prime_p)
-                
 
-    def create_key(self):
-        pass
+            elif cmd == "1":
+                self.key_components = self.key.create_key()
+            
+            elif cmd == "2":
+                p = int(input("1. alkuluku: "))
+                q = int(input("2. alkuluku: "))
+                self.key_components = self.key.create_key(p,q)
+
+            elif cmd == "3":
+                self.print_key_info()
+
+    def print_key_info(self):
+        print()
+        print("d: ", self.key.secret_key_d)
+        print("n: ", self.key.public_key_n)
+        print("e: ", self.key.public_key_e)
+        print("p: ", self.key.prime_p)
+        print("q: ", self.key.prime_q)
+        print("ln:", self.key.ln)
+                        
+class Key:
+    def __init__(self):
+        self.secret_key_d = None
+        self.public_key_n = None
+        self.public_key_e = None
+        self.prime_p = None
+        self.prime_q = None
+        self.ln = None
+
+    def create_key(self, p=None, q=None):
+        # 1. Choose two primes p and q
+        if not p:
+            self.choose_primes()
+        else:
+            self.prime_p = p
+            self.prime_q = q
+        # 2. Calculate n = pq
+        self.public_key_n = self.prime_p * self.prime_q
+        # 3. Calculate lambda(n) = ln using Charmichael function
+        self.carmichael_function()
+        # 4. Choose e that is coprime with ln
+        self.choose_e()
+
+    def choose_primes(self):
+        self.prime_p = Prime().random_prime()
+        self.prime_q = Prime().random_prime()
+    
+    # Carmichael function calculates lambda(n) = lcm(p-1, q-1)
+    # lcm(a, b) = abs(ab) / gcd(a,b)
+    def carmichael_function(self):
+        gcd = self.euclidean_algorithm()
+        abs = abs((self.prime_p - 1) * (self.prime_q - 1))
+        self.ln = abs // gcd
+
+    # Euclidean algorithm solves the greatest common divisor gcd
+    def euclidean_algorithm(self):
+        a = self.prime_p - 1
+        b = self.prime_q - 1
+        while True:
+            if a > b:
+                a = a % b
+            else:
+                b = b % a
+            if a == 0:
+                return b
+            if b == 0:
+                return a
+    
+    # e is coprime with ln if e is prime and it does not divide ln
+    def choose_e(self):
+        while True:
+            e = Prime().random_prime(1, self.ln)
+            if self.ln % e == 0:
+                self.public_key_e = e
 
 class Prime:
-    def __init__(self):
-        pass
-
     # Used for testing
     first_58_primes = [
             2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,
@@ -46,23 +107,15 @@ class Prime:
         i = 2
         while i*i <= nro:
             if nro % i == 0:
-                #print(nro, "on jaollinen luvulla", i)
                 return False
             i += 1
         return True
 
-    # returns the next prime beginning from p
-    def next_prime(self, p:int):
-        while True:
-            if self.trial_division(p):
-                return p
-            p += 1
-
-    # returns a random prime number between a and the first prime after b
+    # returns a random prime number between a and b
     def random_prime(self, a=10**12, b=10**13):
-        n = randint(a, b)
-        prime = self.next_prime(n)
-        return prime
-
+        while True:
+            n = randint(a, b)
+            if self.trial_division(n):
+                return n
 
 Program()
