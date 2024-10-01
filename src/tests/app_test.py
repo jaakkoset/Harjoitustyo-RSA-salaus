e38b95751d5b9f889531f47d51fbabb1a2a1b44b
Jaakko Airikkala <jaakkel@gmail.com>
2024-10-02 01:10:08 +0300

    Add first tests

--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -5,6 +5,18 @@ class TestPrime(unittest.TestCase):
     def setUp(self):
         print("Set up goes here")
 
-    #def test_eratosthenes_sieve(self):
-    def test_hello_world(self):
-        self.assertEqual("Hello world", "Hello world")
+    def test_eratosthenes_sieve_up_to_271(self):
+        prime = Prime()
+        list = prime.eratosthenes_sieve(271)
+        self.assertEqual(list, prime.first_58_primes)
+
+    def test_factor_twos(self):
+        prime = Prime()
+        a = prime.factor_twos(1408)
+        b = prime.factor_twos(512)
+        c = prime.factor_twos(6272)
+        d = prime.factor_twos(2)
+        self.assertEqual(a, (7, 11))
+        self.assertEqual(b, (9, 1))
+        self.assertEqual(c, (7, 49))
+        self.assertEqual(d, (1, 1))
