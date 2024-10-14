6ee16ce80eea409519ecf08d5863f36387a38ad2
Jaakko Airikkala <jaakkel@gmail.com>
2024-10-14 18:39:55 +0300

    Use hexadecimals when turning text to integers

--- src/message.py
+++ src/message.py
@@ -1,3 +1,6 @@
+import base64
+
+
 class Message:
     """This class has methods to encrypt and decrypt messages"""
 
@@ -10,29 +13,28 @@ class Message:
         return pow(cipher, d, n)
 
     def text_to_integer(self, text: str):
-        """Turns text into an integer.
-        Finds the ASCII-value for each character and concatenates the values.
-        If ASCII-value is less that 100, a 3 is added on the left side of the
-        number so that all characters get a value three digits long. Largest
-        ASCII-value is 255, so this creates no problems."""
-        integer = ""
-        for character in text:
-            a = ord(character)
-            if a > 255:
-                raise ValueError(f"A non-ASCII character used: {character}")
-            if a < 100:
-                a = "3" + str(a)
-            integer = integer + str(a)
-        return int(integer)
+        """Turns text into an integer using UTF-8 encoding.
+        UTF-8 is roughly speaking the same as extended ASCII"""
+        # Turn the text into a bytes-like object and define the encoding as UTF-8.
+        text = bytes(text, "utf-8")
+        # Give each character its hexadecimal UTF-8 value and concatenate them into one
+        # integer.
+        base16_text = base64.b16encode(text)
+        # Turn the hexadecimal integer into a decimal number
+        base16_text = int(base16_text, 16)
+        return base16_text
 
     def integer_to_text(self, integer: int):
         """Turns integers back to text."""
-        text = ""
-        integer = str(integer)
-        for i in range(0, len(integer) - 2, 3):
-            ascii = ""
-            if int(integer[i]) != 3:
-                ascii = ascii + integer[i]
-            ascii = ascii + integer[i + 1] + integer[i + 2]
-            text = text + chr(int(ascii))
+        # Turn the integer into a hexadecimal
+        base16 = hex(integer)
+        # Turn it into a string
+        base16 = str(base16)
+        # Hexadecimals have 0x in the beginning, so we remove that.
+        base16 = base16[2:]
+        # Decode the hexadecimal into UTF-8 characters. This returns a byte-like
+        # object.
+        text = base64.b16decode(base16, casefold=True)
+        # Turn the answer into string
+        text = str(text, "utf-8")
         return text
