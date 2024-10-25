2aaea8540a086809707144e9c37d3129e85fc87a
Jaakko Airikkala <jaakkel@gmail.com>
2024-10-26 00:20:34 +0300

    Edit comments and names in encryption_test.py

--- src/tests/encryption_test.py
+++ src/tests/encryption_test.py
@@ -3,7 +3,7 @@ from message import Message
 from generator import Generator
 
 
-class TestMessage(unittest.TestCase):
+class TestGenerator(unittest.TestCase):
     def setUp(self):
         self.msg = Message()
         self.generator = Generator()
@@ -12,27 +12,29 @@ class TestMessage(unittest.TestCase):
         """Tests encryption with a 1024 bit key. The length of the encrypted text is 128
         characters, which is the maximum for this key length."""
         text = self.open_file(128)
-        self.encrypt_decrypt_encrypt(1024, text)
+        self.encrypt_decrypt(1024, text)
 
     def test_encryption_1024_medium(self):
         """Tests encryption with a 1024 bit key. The length of the encrypted text is 50
         characters."""
         text = "abcdefghijklmnopqrstuvwxyzåäö 012456789,.!%&/()=?"
-        self.encrypt_decrypt_encrypt(1024, text)
+        self.encrypt_decrypt(1024, text)
 
     def test_encryption_1024_quantum(self):
         """Tests encryption with a 1024 bit key. The length of the encrypted text is
         1 character."""
         text = "a"
-        self.encrypt_decrypt_encrypt(1024, text)
+        self.encrypt_decrypt(1024, text)
 
-    def test_encrypt_decrypt_encrypt_2048(self):
+    def test_encryption_2048(self):
         """Tests encryption with a 2048 bit key. The length of the encrypted text is the
         maximum for this key length, 256 characters."""
         text = self.open_file(256)
-        self.encrypt_decrypt_encrypt(2048, text)
+        self.encrypt_decrypt(2048, text)
 
-    def encrypt_decrypt_encrypt(self, bits: int, text: str):
+    def encrypt_decrypt(self, bits: int, text: str):
+        """Helper function. Encrypts the given text and then decrypts it. Then asserts
+        that that the message has remained the same."""
         key = self.generator.create_key(bits)
         integer = self.msg.text_to_integer(text)
         encrypted = self.msg.encrypt(integer, key["e"], key["n"])
@@ -42,6 +44,7 @@ class TestMessage(unittest.TestCase):
         self.assertEqual(message, text)
 
     def open_file(self, text_length: int):
+        """Opens files text-128.txt and text-256.txt"""
         with open("src/tests/data/" + "text-" + str(text_length) + ".txt") as file:
             text = ""
             for row in file:
